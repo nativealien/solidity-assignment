8eb148bff6bd19c7589c08f0cddb2b95bbdc4598
nativealien <oskar@assarsson.nu>
2024-09-05 06:22:10 +0200

    added basic functions, with basic tests

--- contracts/BlockNotes.sol
+++ contracts/BlockNotes.sol
@@ -2,7 +2,26 @@
 pragma solidity ^0.8.26;
 
 contract BlockNotes {
+    uint256 public noteCount;
+    
+    struct Note {
+        uint256 id;
+        string content;
+    }
+
+    mapping(uint256 => Note) public notes;
+
     constructor() {
         noteCount = 0;
+    }
+
+    function createNote(string memory _content) public {
+        noteCount++;
+        notes[noteCount] = Note(noteCount, _content);
+    }
+
+    function getNote(uint256 _id) public view returns (string memory) {
+        require(_id > 0 && _id <= noteCount, "Note does not exist.");
+        return notes[_id].content;
     }
 }
---
+++ test/BlockNotes.ts
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import { ContractTransactionResponse } from "ethers";
+import { ethers } from "hardhat";
+import { BlockNotes } from "../typechain-types";
+
+describe("BlockNotes", function () {
+  let BlockNotes, blockNotes: BlockNotes & { deploymentTransaction(): ContractTransactionResponse; };
+
+  beforeEach(async function () {
+    BlockNotes = await ethers.getContractFactory("BlockNotes");
+    blockNotes = await BlockNotes.deploy();
+  });
+
+  it("Should start with a note count of 0", async function () {
+    const noteCount = await blockNotes.noteCount();
+    expect(noteCount).to.equal(0);
+  });
+
+  it("Should allow a user to create a note", async function () {
+    await blockNotes.createNote("First note");
+    const noteCount = await blockNotes.noteCount();
+    expect(noteCount).to.equal(1);
+
+    const note = await blockNotes.getNote(1);
+    expect(note).to.equal("First note");
+  });
+
+  it("Should return the correct note text", async function () {
+    await blockNotes.createNote("First note");
+    await blockNotes.createNote("Second note");
+
+    const firstNote = await blockNotes.getNote(1);
+    const secondNote = await blockNotes.getNote(2);
+
+    expect(firstNote).to.equal("First note");
+    expect(secondNote).to.equal("Second note");
+  });
+
+  it("Should fail if note doesnt exist", async function () {
+    await expect(blockNotes.getNote(999)).to.be.rejectedWith("Note does not exist.");
+  });
+});
