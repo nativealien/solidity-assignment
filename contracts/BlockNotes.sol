424e6a6fa40f9aa0dcd5372d35d4f980748b2f6d
nativealien <oskar@assarsson.nu>
2024-09-05 11:46:00 +0200

    added event and modifiers

--- contracts/BlockNotes.sol
+++ contracts/BlockNotes.sol
@@ -15,6 +15,20 @@ contract BlockNotes {
     mapping(uint256 => Note) private notes;
     uint256 private noteCount;
 
+    event NoteCreated(uint256 _noteId, string _content, address _owner, Visibility _visibility);
+
+    modifier onlyOwner(uint256 _noteId) {
+        require(msg.sender == notes[_noteId].owner, "Not the owner of this note");
+        _;
+    }
+
+    modifier noteExist(uint256 _noteId) {
+        require(_noteId > 0 && 
+                _noteId <= noteCount && 
+                notes[_noteId].exists == true, "Note does not exist");
+        _;
+    }
+
     constructor() {
         noteCount = 0;
     }
@@ -27,29 +41,39 @@ contract BlockNotes {
         notes[noteCount].owner = msg.sender;
         notes[noteCount].exists = true;
         notes[noteCount].visibility = _visibility;
+
+        emit NoteCreated(noteCount, _content, msg.sender, _visibility);
     }
 
-    function getNote(uint256 _noteId) public view returns (string memory) {
-        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
-        require(msg.sender == notes[_noteId].owner || notes[_noteId].sharedWith[msg.sender] == true || notes[_noteId].visibility == Visibility.Public, "Not authorized to read the note");
+    function getNote(uint256 _noteId) public 
+        noteExist(_noteId)
+        view returns (string memory) {
+
+        require(msg.sender == notes[_noteId].owner || 
+                notes[_noteId].sharedWith[msg.sender] == true || 
+                notes[_noteId].visibility == Visibility.Public, "Not authorized to read the note");
+
         return notes[_noteId].content;
     }
 
-    function shareWith(uint256 _noteId, address _user) public {
-        require(msg.sender == notes[_noteId].owner, "Not the owner");
-        require(notes[_noteId].exists == true, "Note does not exist");
+    function shareWith(uint256 _noteId, address _user) public 
+        onlyOwner(_noteId) 
+        noteExist(_noteId) {
+
         notes[_noteId].sharedWith[_user] = true;
     }
 
-    function deleteNote(uint256 _noteId) public {
-        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
-        require(msg.sender == notes[_noteId].owner, "Not the owner of this note");
+    function deleteNote(uint256 _noteId) public 
+        onlyOwner(_noteId) 
+        noteExist(_noteId) {
+
         delete notes[_noteId];
     }
 
-    function changeVisibility(uint256 _noteId, Visibility newVisibility) public {
-        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
-        require(msg.sender == notes[_noteId].owner, "Not the owner of this note");
+    function changeVisibility(uint256 _noteId, Visibility newVisibility) public 
+        onlyOwner(_noteId) 
+        noteExist(_noteId) {
+
         notes[_noteId].visibility = newVisibility;
     }
 }
