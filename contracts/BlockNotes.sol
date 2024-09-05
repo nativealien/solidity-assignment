5655f7647f19bb3977a7588d09d87ffd7df8f691
nativealien <oskar@assarsson.nu>
2024-09-05 11:06:41 +0200

    added delete and change visibility with tests, some refactoring

--- contracts/BlockNotes.sol
+++ contracts/BlockNotes.sol
@@ -7,6 +7,7 @@ contract BlockNotes {
     struct Note {
         string content;
         address owner;
+        bool exists;
         Visibility visibility;
         mapping(address => bool) sharedWith;
     }
@@ -24,21 +25,31 @@ contract BlockNotes {
         noteCount++;
         notes[noteCount].content = _content;
         notes[noteCount].owner = msg.sender;
+        notes[noteCount].exists = true;
         notes[noteCount].visibility = _visibility;
     }
 
     function getNote(uint256 _noteId) public view returns (string memory) {
-        require(_noteId > 0 && _noteId <= noteCount, "Note does not exist");
+        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
+        require(msg.sender == notes[_noteId].owner || notes[_noteId].sharedWith[msg.sender] == true || notes[_noteId].visibility == Visibility.Public, "Not authorized to read the note");
         return notes[_noteId].content;
     }
 
     function shareWith(uint256 _noteId, address _user) public {
         require(msg.sender == notes[_noteId].owner, "Not the owner");
+        require(notes[_noteId].exists == true, "Note does not exist");
         notes[_noteId].sharedWith[_user] = true;
     }
 
-    function isNoteSharedWith(uint256 _noteId, address _user) public view returns (bool) {
-        require(_noteId > 0 && _noteId <= noteCount, "Note does not exist");
-        return notes[_noteId].sharedWith[_user];
+    function deleteNote(uint256 _noteId) public {
+        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
+        require(msg.sender == notes[_noteId].owner, "Not the owner of this note");
+        delete notes[_noteId];
+    }
+
+    function changeVisibility(uint256 _noteId, Visibility newVisibility) public {
+        require(_noteId > 0 && _noteId <= noteCount && notes[_noteId].exists == true, "Note does not exist");
+        require(msg.sender == notes[_noteId].owner, "Not the owner of this note");
+        notes[_noteId].visibility = newVisibility;
     }
 }
--- test/BlockNotes.ts
+++ test/BlockNotes.ts
@@ -5,12 +5,12 @@ import { BlockNotes } from "../typechain-types";
 
 describe("BlockNotes", function () {
   let BlockNotes, blockNotes: BlockNotes & { deploymentTransaction(): ContractTransactionResponse; };
-  let accounts: (Typed | AddressLike)[];
+  let owner, addr1: any, addr2: any;
 
   beforeEach(async function () {
     BlockNotes = await ethers.getContractFactory("BlockNotes");
+    [owner, addr1, addr2]  = await ethers.getSigners()
     blockNotes = await BlockNotes.deploy();
-    accounts = await ethers.getSigners()
   });
 
   it("should create a note with visibility", async () => {
@@ -33,16 +33,43 @@ describe("BlockNotes", function () {
 
   it("should allow user to share notes with other users", async () => {
     await blockNotes.createNote("Shared note", 0);
-    await blockNotes.shareWith(1, accounts[1]);
-    const isShared = await blockNotes.isNoteSharedWith(1, accounts[1])
-    expect(isShared).to.be.true
+    await blockNotes.shareWith(1, addr1);
+    const note = await blockNotes.connect(addr1).getNote(1);
+    expect(note).to.equal("Shared note")
   })
 
-  it("should not share with other users", async () => {
+  it("should not share with unauthorized users", async () => {
     await blockNotes.createNote("Shared note", 0);
-    await blockNotes.shareWith(1, accounts[1]);
-    const isShared = await blockNotes.isNoteSharedWith(1, accounts[2])
-    expect(isShared).to.be.false
+    await blockNotes.shareWith(1, addr1);
+
+    await expect(blockNotes.connect(addr2).getNote(1))
+        .to.rejectedWith("Not authorized to read the note")
+  })
+
+  it("should be availible to all users if public", async () => {
+    await blockNotes.createNote("Public note", 2);
+    const msg1 = await blockNotes.connect(addr1).getNote(1)
+    const msg2 = await blockNotes.connect(addr2).getNote(1)
+
+    expect(msg1 && msg2).to.equal("Public note")
+  })
+
+  // Change and delete...
+
+  it("should allow the owner to delete the note", async () => {
+    await blockNotes.createNote("Note to delete", 0);
+    await blockNotes.deleteNote(1);
+    await expect(blockNotes.getNote(1)).to.rejectedWith("Note does not exist")
+  })
+
+  it("should allow user to change visibility", async () => {
+    await blockNotes.createNote("Note to change", 0);
+    await blockNotes.changeVisibility(1, 2);
+
+    const msg1 = await blockNotes.connect(addr1).getNote(1)
+    const msg2 = await blockNotes.connect(addr2).getNote(1)
+
+    expect(msg1 && msg2).to.equal("Note to change")
   })
 
 });
