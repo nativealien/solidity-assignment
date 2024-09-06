2d63c4468a6f314f2ab0154523a8926cadd277ff
nativealien <oskar@assarsson.nu>
2024-09-06 21:02:16 +0200

    added to hardhat config for sepolia deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-etherscan';
 import "@nomicfoundation/hardhat-chai-matchers";
 import "solidity-coverage";
+import { config as envconfig } from 'dotenv';
+
+envconfig()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.26",
@@ -10,7 +14,16 @@ const config: HardhatUserConfig = {
     tests: "./test",
     cache: "./cache",
     artifacts: "./artifacts"
-  }
+  },
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
 };
 
 export default config;
